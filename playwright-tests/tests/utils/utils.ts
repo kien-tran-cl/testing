131a9415eaddb77cdbb4b6acb1672f9577af8764
kien-tran-cl <e-kien.tran@coduct.com>
2024-12-05 11:34:54 +0700

    Update utils.ts

--- playwright-tests/tests/utils/utils.ts
+++ playwright-tests/tests/utils/utils.ts
@@ -1,81 +1,61 @@
-import fetch from 'node-fetch';
-import { config } from 'dotenv';
-config(); // Load .env file
-
-// Type definition for a single message
-export type ApiMessage = {
-  id: string;
-  status: string;
-  type: string;
-  recipient: string;
-  body: string;
-  subject: string;
-  template_type: string;
-  channel: string;
-  send_count: number;
-  created_at: string;
-  updated_at: string;
-};
-
-// Type definition for the API response (an array of messages)
-export type ApiResponse = ApiMessage[];
+import { Page, Locator } from 'playwright/test';
 
 /**
- * Fetch OTP code from API
- * @param recipient The email address to fetch the OTP for
- * @returns OTP code as a string
+ * Helper class to interact with the page.
+ * @see https://playwright.dev/docs/actionability
  */
-export async function fetchOtpCode(recipient: string): Promise<string> {
-  const apiUrl = process.env.OTP_API_URL;
-  const apiKey = process.env.OTP_API_KEY;
+export class UI {
+  private constructor(private readonly page: Page) {}
 
-  if (!apiUrl || !apiKey) {
-    throw new Error(
-      'OTP API URL or Key is not defined in the environment variables.',
-    );
+  static of(page: Page): UI {
+    return new UI(page);
   }
 
-  const headers = {
-    Authorization: `Bearer ${apiKey}`,
-    Accept: 'application/json',
-  };
+  getBy(selector: string): Locator {
+    return this.page.locator(selector);
+  }
 
-  try {
-    // Send a GET request to the API
-    const response = await fetch(
-      `${apiUrl}?status=sent&recipient=${encodeURIComponent(recipient)}`,
-      {
-        method: 'GET',
-        headers,
-      },
-    );
+  getAllBy(selector: string): Promise<Locator[]> {
+    return this.getBy(selector).all();
+  }
 
-    // Check if the request was successful
-    if (!response.ok) {
-      throw new Error(`Failed to fetch OTP: ${response.statusText}`);
+  getById(id: string): Locator {
+    return this.getBy(`#${id}`);
+  }
+
+  getByClassName(className: string): Locator {
+    return this.getBy(`.${className}`);
+  }
+
+  fill(selector: string, value: string): Promise<void> {
+    return this.getBy(selector).fill(value);
+  }
+
+  async type(
+    selector: string,
+    value: string,
+    clearBeforeType: boolean = true,
+  ): Promise<void> {
+    if (clearBeforeType) {
+      await this.clear(selector);
     }
+    await this.click(selector);
+    await this.page.keyboard.type(value);
+  }
 
-    // Parse the response and assert its type
-    const data = (await response.json()) as ApiResponse;
+  clear(selector: string): Promise<void> {
+    return this.getBy(selector).clear();
+  }
 
-    // Ensure the response contains at least one message
-    if (data.length === 0) {
-      throw new Error('No messages found for the recipient.');
-    }
+  click(selector: string): Promise<void> {
+    return this.page.click(selector);
+  }
 
-    // Get the latest message
-    const latestMessage = data[0];
+  async getInnerText(selector: string): Promise<string> {
+    return (await this.getBy(selector).innerText()).trim();
+  }
 
-    // Extract OTP from the subject using a regex
-    const otpMatch = RegExp(/Login-Code (\d{6})/).exec(latestMessage.subject);
-    if (!otpMatch) {
-      throw new Error('Failed to extract OTP from the subject.');
-    }
-
-    // Return the extracted OTP
-    return otpMatch[1];
-  } catch (error) {
-    console.error('Error fetching OTP:', error);
-    throw error;
+  waitForSeconds(seconds: number): Promise<void> {
+    return this.page.waitForTimeout(seconds * 1000);
   }
 }
