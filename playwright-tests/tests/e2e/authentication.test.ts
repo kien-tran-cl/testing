253a44332090188a058fd8b9675fbb23af881a49
kien-tran-cl <e-kien.tran@coduct.com>
2024-12-05 10:51:43 +0700

    Update authentication.test.ts

--- playwright-tests/tests/e2e/authentication.test.ts
+++ playwright-tests/tests/e2e/authentication.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { activitiesSelectors } from '../utils/selectors';
-import { UI } from '../utils';
+import { UI } from '../utils/utils';
 import { login, verifyOtp } from '../common';
 
 const { USER_EMAIL } = process.env;
