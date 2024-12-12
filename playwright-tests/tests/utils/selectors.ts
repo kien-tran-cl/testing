b8e2c28bc432ea740206699ee137b0910c0fa9cf
kien-tran-cl <e-kien.tran@coduct.com>
2024-12-12 14:39:01 +0700

    Update warning message selectors

--- playwright-tests/tests/utils/selectors.ts
+++ playwright-tests/tests/utils/selectors.ts
@@ -3,22 +3,23 @@ export const loginPageSelectors = {
     logo: 'svg-icon',
     emailInput: 'input[name="identifier"]',
     continueButton: 'button:has-text("Continue")',
-    warningMessage: 'p.text-common-danger',
+    emailWarningMessage: '.p-error.block.text-xs.translate-x-0',
+    tenantWarningMessage: 'p.text-common-danger',
     legalNotice: 'a:has-text("Legal Notice")',
     termsOfService: 'a:has-text("Terms of Service")',
     dataPrivacy: 'a:has-text("Data Privacy")',
     copyright: 'text=©',
   };
   
-  export const loginVerificationSelectors = {
+export const loginVerificationSelectors = {
     title: 'h4:has-text("Login verification")',
     emailInfo: 'h5',
     otpInput: '.p-inputotp-input',
+    otpWarningMessage: 'span.p-error',
     resendButton: 'button:has-text("Resend")',
-    submitButton: 'button:has-text("Continue")',
   };
   
-  export const activitiesSelectors = {
+export const activitiesSelectors = {
     headerTitle: 'gedat-home-header',
   };
   
