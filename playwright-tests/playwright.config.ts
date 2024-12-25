3e674e4d5af65248f9f032dc114075706f587de6
kien-tran-cl <e-kien.tran@coduct.com>
2024-12-25 14:14:58 +0700

    Run test on Chrome Mobile only

--- playwright-tests/playwright.config.ts
+++ playwright-tests/playwright.config.ts
@@ -55,9 +55,9 @@ export default defineConfig({
       use: { ...devices['Pixel 5'] },
     },
     {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-    },
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
 
     /* Test against branded browsers. */
     // {
