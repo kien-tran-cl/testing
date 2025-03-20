7cd57e71f49c962adcff3aa21e4bfd8ac9681431
Kien CodeLeap <kiencodeleap@Kiens-MacBook-Pro.local>
2025-03-20 13:33:14 +0700

    Update playwright.config.ts: Enable parallel testing on CI with optimized worker limit

--- playwright-tests/playwright.config.ts
+++ playwright-tests/playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+import os from "os";
 
 /**
  * Read environment variables from file.
@@ -20,8 +21,10 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Limit the number of workers on CI */
+  workers: process.env.CI ? Math.min(4, os.cpus().length) : undefined, 
+  // Limits workers to 4 or the number of available CPUs, whichever is smaller.
+  // This helps balance speed and resource usage to avoid overloading CI.
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
